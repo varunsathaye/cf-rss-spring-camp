dfae1249d4c9a3fd96ba85c5df4f0dd0edc6a05c
varunsathaye <153094795+varunsathaye@users.noreply.github.com>
2024-02-01 20:14:25 +0530

    Update worker.go

--- worker/worker.go
+++ worker/worker.go
@@ -5,6 +5,10 @@ import "mongo-with-golang/stores"
 func PerformWork(m *stores.MongoStore){
 	for{
 		m.OpenConnectionWithMongoDB()
+		final_data, _ := (cfclient.RecentActions(5))
+		m.StoreRecentActionsInTheDatabase(final_data)
+		delay := 5 * time.Minute
+		<-time.After(delay)
 		
 	}
 }
